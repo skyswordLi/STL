513cc79cdcad73de586901f4ded548fa814ce55a
skyswordLi <514226621@qq.com>
2016-10-05 11:19:20 +0800

    Modified some coding styles.

--- chap1/chap1.cpp
+++ chap1/chap1.cpp
@@ -19,25 +19,25 @@ Thank you "Stephan T. Lavavej"
 #include <functional>
 
 #if TEST1
-/*	 Sequence Container: (maintains the sequence of elements)
-		vector
-		list
-		deque
-		forward_list
+/*   Sequence Container: (maintains the sequence of elements)
+        vector
+        list
+        deque
+        forward_list
 
-	 Vector is sequence container, maintain the sequence of elements
-	 contiguous elements
-	 Resizing will be needed time to time when you push more elment.
+     Vector is sequence container, maintain the sequence of elements
+     contiguous elements
+     Resizing will be needed time to time when you push more elment.
       - do a geometric allocation. new_no_of_element  = k * (previous_no_of_element)
-	  -	k is 1.5 in Visual studio.
+      - k is 1.5 in Visual studio.
       - time is insert an element is "amortized time" constant.
-	  - Don't do new_no_of_element  = k + (previous_no_of_element). This will give quardratic time of insert.
+      - Don't do new_no_of_element  = k + (previous_no_of_element). This will give quardratic time of insert.
 
 iterators
-	 	v.begin(),v.end() == [x,y) --> v.end() is 1  past the array.
+        v.begin(),v.end() == [x,y) --> v.end() is 1  past the array.
         why is this ? Think of empty vector. its begin and end are same. more easy to model empty vector
 
-		http://stackoverflow.com/questions/6673762/why-is-used-with-iterators
+        http://stackoverflow.com/questions/6673762/why-is-used-with-iterators
 
 */
 int main(){
@@ -51,29 +51,29 @@ int main(){
   //v[5] = 100; // undefined behaviour
 
   std::cout << v.size() <<std::endl;
-  for(size_t i = 0; i< v.size(); i++){ // think is v.size() fast enough for all containers... maybe not for list ?
-	std::cout<< v[i]<< " "; // is [] operator overloaded for all conitainers?? Not for list.
+  for(size_t i = 0; i < v.size(); i++){ // think is v.size() fast enough for all containers... maybe not for list ?
+    std::cout<< v[i]<< " "; // is [] operator overloaded for all conitainers?? Not for list.
   }
 
   // use iterators , more generric  way to do the iterations -- which works for all conitainers.
   std::cout<< std::endl;
-  std::vector<int>::iterator i = v.begin() +1 ;
+  std::vector<int>::iterator i = v.begin() + 1 ;
   std::cout<< *i <<std::endl;
   /* All iterators are equality comparable. (defines != et al)
      Only random access iterators are relationally comparable. (like of vector defines < )
      Input iterators, forward iterators, and bidirectional iterators are not relationally comparable. (eg List)
   */
   for(std::vector<int>::iterator i = v.begin(); i != v.end(); ++i){ // notice use of  != rather than < as it is defined for iterators. also notice ++i rather than i++, which is more faster as avoid use of temp
-	std::cout<< *i << " ";
+    std::cout<< *i << " ";
   }
   std::cout<< std::endl;
 
   // sort is not stable (equal elements are not gaurenteed to come out in same order as input)
-  std::sort(v.begin(),v.end()); // Can i use sort for List? No , List does not support random access iterator.
+  std::sort(v.begin(), v.end()); // Can i use sort for List? No , List does not support random access iterator.
 
   /* auto make i same as the one return by v.begin() */
   for(auto i = v.begin(); i != v.end(); ++i){ // need --std=c++0x in while compiling.
-	std::cout<< *i << " ";
+    std::cout<< *i << " ";
   }
   std::cout<< std::endl;
 
@@ -83,31 +83,33 @@ int main(){
   sv.push_back("antelope");
   sv.push_back("puppy");
   sv.push_back("bear");
-  sort(sv.begin(),sv.end(),std::greater<std::string>()); //third argument is function object or lambda expression
+  sort(sv.begin(), sv.end(), std::greater<std::string>()); //third argument is function object or lambda expression
 
 
-  for(auto i = sv.begin(); i != sv.end(); ++i){ // need --std=c++0x in cmd line  while compiling.
-  	std::cout<< *i  << " ";
+  for(auto i = sv.begin(); i != sv.end(); ++i) { // need --std=c++0x in cmd line  while compiling.
+    std::cout<< *i  << " ";
   }
   std::cout<< std::endl;
 
-   sort(sv.begin(),sv.end(),
-	   [](const std::string& left,const std::string &right){
-		 return left.size() < right.size();
-  		}
+  sort(sv.begin(),sv.end(),
+       [](const std::string& left,const std::string &right)
+       {
+         return left.size() < right.size();
+       }
   ); // lambda expression
-  for(auto i = sv.begin(); i != sv.end(); ++i){ // need --std=c++0x in cmd line  while compiling.
-  	std::cout<< *i  << " ";
+  for(auto i = sv.begin(); i != sv.end(); ++i) { // need --std=c++0x in cmd line  while compiling.
+    std::cout<< *i  << " ";
   }
   std::cout<< std::endl;
 
   stable_sort(sv.begin(),sv.end(),
-	   [](const std::string& left,const std::string &right){
-		 return left.size() < right.size();
-  		}
+       [](const std::string& left,const std::string &right)
+        {
+         return left.size() < right.size();
+        }
   ); // mantain the order of equal element. Its a stable version. Less efficient than sort ()
   for(auto i = sv.begin(); i != sv.end(); ++i){ // need --std=c++0x in cmd line  while compiling.
-  	std::cout<< *i  << " ";
+    std::cout<< *i  << " ";
   }
   std::cout<< std::endl;
 
@@ -116,18 +118,17 @@ int main(){
   // dont use erase(). vector shift all the element. total runtime  O(n^2)
   // use std::remove()
   v.erase(remove_if(v.begin(),v.end(),
-					[](int e){
-					  return (e % 2 == 1);
-						     })
-           ,v.end()
-		  );
+                    [](int e)
+                    {
+                      return (e % 2 == 1);
+                    }
+                   ), v.end()
+          );
 
-  for(auto i = v.begin(); i != v.end(); ++i){ // need --std=c++0x in cmd line  while compiling.
-  	std::cout<< *i  << " ";
+  for(auto i = v.begin(); i != v.end(); ++i) { // need --std=c++0x in cmd line  while compiling.
+    std::cout<< *i  << " ";
   }
   std::cout<< std::endl;
-
-
 
 }
 #endif
